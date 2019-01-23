c6432df96cca0e9fe49e0a443ce5b27b346f56dd
Oscar Miniet <ominiet96@hotmail.com>
2019-01-23 11:42:48 -0500

    needs work to get babel working

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Test App!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
